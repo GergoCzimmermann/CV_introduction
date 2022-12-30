ee3c4e1e0591feee4eca13b16b5420a8542985ad
Gergo <{94219827}+{GergoCzimmermann}@users.noreply.github.com>
2022-12-30 16:29:55 +0100

    Theme mode change JS function development

---
+++ scripts/themeColor.js
@@ -0,0 +1,30 @@
+console.log("The page has been loaded succesfully")
+
+let theme = localStorage.getItem('theme');
+
+if(theme == null){
+    console.log("First theme setting")
+    setTheme('bright');
+}else{
+    setTheme(theme);
+    console.log("Basic theme setting from Local storage")
+}
+
+let themeOptions = document.getElementsByClassName('themeMode');
+
+for (i=0; themeOptions.length>i ; i++){
+    themeOptions[i].addEventListener ('click', function(){
+        let mode = this.dataset.mode;
+        console.log('The following theme has been set: ', mode);
+        setTheme(mode);
+    })
+}
+
+function setTheme(mode){
+    switch (mode) {
+        case "bright": document.getElementById('theme-style').href = "./styles/style.css"; break;
+        case "dark": document.getElementById('theme-style').href = "./styles/darkTheme.css"; break;
+    }
+    
+    localStorage.setItem('theme', mode)
+}
