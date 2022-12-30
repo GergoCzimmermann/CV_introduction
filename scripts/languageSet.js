a232c5123fc54c2b15ad45a8ce4b94e8ecee5980
Gergo <{94219827}+{GergoCzimmermann}@users.noreply.github.com>
2022-12-30 19:02:50 +0100

    Theme selector background color settings during change, Language settings function development

---
+++ scripts/languageSet.js
@@ -0,0 +1,30 @@
+let language = localStorage.getItem('language');
+
+if(language == null){
+    console.log("First language settings")
+    setTheme('hungarian');
+}
+else
+{
+    setTheme(language);
+    console.log("Basic language setting from Local storage")
+}
+
+let languagesettings = document.getElementsByClassName('languageSet');
+
+for (i=0; languagesettings.length>i ; i++){
+    languagesettings[i].addEventListener ('click', function(){
+        let languageType = this.dataset.mode;
+        console.log('The following language has been set: ', languageType);
+        setLanguage(languageType);
+    })
+}
+
+function setLanguage(languageType){
+    switch (languageType) {
+        case "hungarian": document.getElementById('language-style').href = "./styles/languageHun.css"; break;
+        case "english": document.getElementById('language-style').href = "./styles/languageEn.css"; break;
+    }
+    
+    localStorage.setItem('language', languageType)
+}
